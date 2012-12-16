b5afff300db1be7e9f6a1eb1c22c8bab67c60532
vlady <vladut.mail@gmail.com>
2012-12-15 21:28:35 -0800

    Question 3 Week 1: Problem statement

---
+++ week1/question3/question3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void read_input() {
+  freopen("edges.txt", "r", stdin);
+  fclose(stdin);
+}
+
+int main() {
+  read_input();
+  return 0;  
+}
---
+++ week1/question3/run.sh
@@ -0,0 +1,3 @@
+rm a.out
+g++ -O2 question3.cpp
+./a.out
